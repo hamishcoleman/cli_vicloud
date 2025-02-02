88ea9b49cb709e20e709f3ae5acdd8b5e04ccab0
Hamish Coleman <hamish@zot.org>
2025-02-03 10:46:59 +1100

    Add another iam command

--- lib/aws/iam.py
+++ lib/aws/iam.py
@@ -11,3 +11,10 @@ class list_users(base, aws._data_two_deep):
     single_region = True
     r1_key = "Users"
     r2_id = "UserId"
+
+class list_roles(base, aws._data_two_deep):
+    datatype = "aws.iam.list_roles"
+    operator = "list_roles"
+    single_region = True
+    r1_key = "Roles"
+    r2_id = "RoleId"
