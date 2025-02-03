bc90ce4b873c81ba25ba1d2c047cd5bc161dd995
Hamish Coleman <hamish@zot.org>
2025-02-03 16:29:21 +1100

    Show all listeners
    Sadly, there is no API for just fetching the listeners, you must provide
    the ARN for the Load Balancer you wish to get the listeners from.
    
    Thus, we first get the list of all load balancers and then iterate
    through them to get all the listeners

--- lib/aws/elb.py
+++ lib/aws/elb.py
@@ -5,6 +5,36 @@ class base(aws.base):
     service_name = "elbv2"
 
 
+class listeners(base):
+    datatype = "aws.elbv2.listeners"
+
+    def _fetch_one_client(self, client):
+        # first, get the list of load_balancers
+        handler = load_balancers()
+        handler.verbose = self.verbose
+        loadbalancers = handler._fetch_one_client(client)
+
+        arns = set()
+        for _id, elb in loadbalancers.items():
+            arns.add(elb["LoadBalancerArn"])
+
+        r1_key = "Listeners"
+        r2_id = "ListenerArn"
+
+        data = {}
+        for arn in arns:
+            for r1 in self._paged_op(
+                    client,
+                    "describe_listeners",
+                    LoadBalancerArn=arn
+            ):
+                for r2 in r1[r1_key]:
+                    _id = r2[r2_id]
+                    data[_id] = r2
+
+        return data
+
+
 class load_balancers(base, aws._data_two_deep):
     datatype = "aws.elbv2.load_balancers"
     operator = "describe_load_balancers"
