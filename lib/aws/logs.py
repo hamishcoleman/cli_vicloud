385868911a0586f356e855b2764ccd43c71b8ae7
Hamish Coleman <hamish@zot.org>
2025-02-17 13:21:24 +1100

    Allow viewing of event logs

--- lib/aws/logs.py
+++ lib/aws/logs.py
@@ -5,6 +5,26 @@ class base(aws.base):
     service_name = "logs"
 
 
+class events(base):
+    datatype = "aws.logs.events"
+    operator = "get_log_events"
+    params = ['log_group_name', 'log_stream_name']
+    r1_key = "events"
+
+    def _fetch_one_client(self, client, args=None):
+        data = {}
+
+        self._log_fetch_op(client, self.operator)
+
+        _id = 0
+        for r1 in self._paged_op(client, self.operator, logGroupName=args.log_group_name, logStreamName=args.log_stream_name):
+            for r2 in r1[self.r1_key]:
+                data[_id] = r2
+                _id += 1
+
+        return data
+
+
 class groups(base, aws._data_two_deep):
     datatype = "aws.logs.groups"
     operator = "describe_log_groups"
@@ -12,7 +32,7 @@ class groups(base, aws._data_two_deep):
     r2_id = "logGroupName"
 
 
-class streams(base, aws._data_two_deep):
+class streams(base):
     datatype = "aws.logs.streams"
     operator = "describe_log_streams"
     params = ['log_group_name']
@@ -30,4 +50,3 @@ class streams(base, aws._data_two_deep):
                 data[_id] = r2
 
         return data
-
