d28d020e99149648d321ddb24d31f3518ada32f3
Hamish Coleman <hamish@zot.org>
2025-03-07 16:28:09 +1100

    An example reporting tool

---
+++ report_sgr.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""Summarise the security group rule yaml files in this dir"""
+#
+# An initial attempt at writing a tool to mine the dumpped data.
+#
+
+import collections
+import glob
+import yaml
+
+
+def str_table(rows, columns, orderby=None):
+    """Given an array of dicts, do a simple table print"""
+    result = list()
+    widths = collections.defaultdict(lambda: 0)
+
+    if len(rows) == 0:
+        # No data to show, be sure not to truncate the column headings
+        for col in columns:
+            widths[col] = len(col)
+    else:
+        for row in rows:
+            for col in columns:
+                if col in row:
+                    widths[col] = max(widths[col], len(str(row[col])))
+
+    for col in columns:
+        if widths[col] == 0:
+            widths[col] = 1
+        result += "{:{}.{}} ".format(col, widths[col], widths[col])
+    result += "\n"
+
+    if orderby is not None:
+        rows = sorted(rows, key=lambda row: row.get(orderby, ""))
+
+    for row in rows:
+        for col in columns:
+            if col in row:
+                data = row[col]
+            else:
+                data = ''
+            if isinstance(data, (list, dict)):
+                data = str(data)
+
+            result += "{:{}} ".format(data, widths[col])
+        result += "\n"
+
+    return ''.join(result)
+
+
+def str_table_columns(rows):
+    """Given an array of dicts, return a set of all column names"""
+
+    columns = set()
+    for row in rows:
+        columns.update(row)
+
+    return columns
+
+
+def main():
+    rules = {}
+
+    for filename in glob.glob("*.yaml"):
+        with open(filename, "r+") as f:
+            raw = yaml.safe_load(f)
+            _id = raw["metadata"]["resourceid"]
+            item = raw["specifics"]
+            rules[_id] = item
+
+    groups = {}
+    for rule in rules.values():
+        group = groups.setdefault(rule["GroupId"], [])
+
+        # Clean up data we dont need to see at the moment
+        del rule["GroupId"]
+        del rule["GroupOwnerId"]
+        del rule["SecurityGroupRuleId"]
+        del rule["SecurityGroupRuleArn"]
+
+        group.append(rule)
+
+    for _id in sorted(groups.keys()):
+        group = groups[_id]
+        print()
+        print("GroupId:", _id)
+
+        columns = sorted(str_table_columns(group))
+        print(str_table(group, columns, orderby="CidrIpv4"))
+
+
+if __name__ == "__main__":
+    main()
